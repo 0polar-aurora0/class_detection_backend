a71187738aef497fef50157b53b880f99885bd65
0polar-aurora0 <1045612802@qq.com>
2024-05-07 00:48:01 +0800

    home fix

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,9 +1,17 @@
+/*
+ * @Author: fuzhenghao
+ * @Date: 2024-04-30 23:20:19
+ * @LastEditTime: 2024-05-07 00:47:19
+ * @LastEditors: fuzhenghao
+ * @Description: 
+ * @FilePath: \class_detection_backend\src\controller\home.ts
+ */
 import { Controller, Get } from '@midwayjs/core';
 
 @Controller('/')
 export class HomeController {
   @Get('/')
   async home() {
-    return 'Hello Midwayjs!';
+    return '管理系统已经启动!';
   }
 }
